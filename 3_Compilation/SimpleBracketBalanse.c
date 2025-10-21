f31e1f8d8e5faf8053ed95db2ff53223e06d5cc0
Дарья Торгова <dtorgova@gmail.com>
2025-10-21 16:57:31 +0300

    Updated HomeTask 3/ Bracket Balanse: now we can choose the type of brackets

--- 3_Compilation/SimpleBracketBalanse.c
+++ 3_Compilation/SimpleBracketBalanse.c
@@ -2,26 +2,34 @@
 #include <stdio.h>
 #include <string.h>
 
-int balance(char*);
+int balance(char*, char, char);
 
 int main(void)
 {
+    printf("Введите через enter виды открывающей и закрывающей скобки, на баланс которых мы будем проверять строку\n");
+    char open;
+    char close;
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%c", &open);
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%c", &close);
+
     printf("Введите проверяемую строку:\n");
     char stringCheck[200];
     fgets(stringCheck, 200, stdin);
     stringCheck[strcspn(stringCheck, "\n")] = '\0';
-    int result = balance(stringCheck);
+
+    int result = balance(stringCheck, open, close);
     if (result == 0)
         printf("Во введенной строке НЕ соблюдается баланс скобок\n");
     else
         printf("Во введенной строке соблюдается баланс скобок\n");
 }
 
-int balance(char* stringCheck)
+int balance(char* stringCheck, char open, char close)
 {
-    char open = '(';
     int countOpen = 0;
-    char close = ')';
     int countClose = 0;
     for (int i = 0; i < strlen(stringCheck); i++) {
         if (stringCheck[i] == open)
